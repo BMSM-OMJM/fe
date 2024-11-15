982c307ee289d6b64590f54bd6b8aaacacea2e75
Han2 <randirao4245@gmail.com>
2024-11-15 15:03:22 +0900

    ADD Main Function Publishing

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useState } from "react";
 
-export default function App() {
+export default function App(useState) {
+  const [bpm, setBpm] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {" "}
+      {/*main*/}
+      {/* logo */}
+      <View>
+        <Image
+          source={require("./assets/logo.png")}
+          style={styles.logo}
+        ></Image>
+      </View>
+      {/* mainFunction */}
+      <View style={styles.main}>
+        {/* firstText */}
+        <View>
+          <Text style={styles.txt}>내 마음은?</Text>
+        </View>
+        {/* heartIcon */}
+        <View style={styles.r}>
+          <Image source={require("./assets/heart.png")}></Image>
+        </View>
+        {/* lastText */}
+        <View>
+          <Text>{bpm}bpm</Text>
+        </View>
+      </View>
+      <View></View>
     </View>
   );
 }
@@ -13,8 +37,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#BFEAFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    position: "absolute",
+    top: 50,
+    left: 7,
+  },
+  main: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  r: {
+    color: "#FFF1C1",
+    borderRadius: 1000,
   },
 });
