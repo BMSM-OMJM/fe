fdf53c917c4224ddc650c4fb6baaf18b623ec97e
Han2 <randirao4245@gmail.com>
2024-11-19 15:01:45 +0900

    ADD Main Function

--- App.js
+++ App.js
@@ -1,34 +1,43 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ActivityIndicator } from "react-native";
 import { useState } from "react";
+import Logo from "./assets/logo.svg";
+import Heart from "./assets/heart.svg";
 
-export default function App(useState) {
+export default function App() {
   const [bpm, setBpm] = useState(0);
+
+  // 두 개의 폰트 로드
+  const [fontsLoaded] = useFonts({
+    MaplestoryOTFLight: require("./assets/fonts/MaplestoryOTFLight.otf"),
+    YOnepickBold: require("./assets/fonts/YOnepick-Bold.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      {" "}
-      {/*main*/}
-      {/* logo */}
-      <View>
-        <Image
-          source={require("./assets/logo.png")}
-          style={styles.logo}
-        ></Image>
+      {/* 로고 */}
+      <View style={styles.logo}>
+        <Logo />
       </View>
-      {/* mainFunction */}
+
+      {/* 메인 기능 */}
       <View style={styles.main}>
-        {/* firstText */}
+        {/* 텍스트 */}
         <View>
-          <Text style={styles.txt}>내 마음은?</Text>
+          <Text style={[styles.txt, styles.onepickFont]}>내 마음은?</Text>
         </View>
-        {/* heartIcon */}
+
+        {/* 하트 아이콘 */}
         <View style={styles.r}>
-          <Image source={require("./assets/heart.png")}></Image>
+          <Heart />
         </View>
-        {/* lastText */}
+
+        {/* BPM 표시 */}
         <View>
-          <Text>{bpm}bpm</Text>
+          <Text style={[styles.bpmText, styles.mapleFont]}>{bpm} bpm</Text>
         </View>
       </View>
+
+      {/* 바텀스크롤 */}
       <View></View>
     </View>
   );
@@ -43,7 +52,7 @@ const styles = StyleSheet.create({
   },
   logo: {
     position: "absolute",
-    top: 50,
+    top: 55,
     left: 7,
   },
   main: {
@@ -53,7 +62,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   r: {
-    color: "#FFF1C1",
+    backgroundColor: "#FFF1C1",
+    padding: 40,
     borderRadius: 1000,
+  },
+  mapleFont: {},
+  bpmText: {
+    fontSize: 50,
+    marginTop: 26,
+  },
+  onepickFont: {
+    fontSize: 50,
+    marginBottom: 13,
   },
 });
